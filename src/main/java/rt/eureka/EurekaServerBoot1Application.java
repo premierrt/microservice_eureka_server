aabe924f205c2cf50b37961d82a9a71185c0d337
Premier R <premierrt@gmail.com>
2018-10-02 17:33:54 +0200

    Logowanie (#4)
    * log1
    
    * push usuniety
    
    * logowanie do pliku ok
    
    * notes
    
    * notatki

--- src/main/java/rt/eureka/EurekaServerBoot1Application.java
+++ src/main/java/rt/eureka/EurekaServerBoot1Application.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerBoot1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerBoot1Application.class, args);
+		log.info("test ");
 	}
 }
